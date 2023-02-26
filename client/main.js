ec40864b6c86872991cbd2f735f09f4ea3712218
soray677 <soray677@outlook.com>
2023-02-26 12:51:41 +0900

    arrow notification button add

--- client/main.js
+++ client/main.js
@@ -1,1 +1,11 @@
 import "./style.css";
+
+// arrow notification
+const arrowPushNotificationButton = document.getElementById(
+  "arrowPushNotificationButton"
+);
+arrowPushNotificationButton?.addEventListener("click", () => {
+  Notification.requestPermission().then((permission) => {
+    console.log(permission);
+  });
+});
